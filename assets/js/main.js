a500d925e37dc66629b9ec6a076b90eafb72b075
Lucas Ferreira <lcklucassocial@gmail.com>
2021-10-15 14:43:16 -0300

    JS Home Page Images Changing

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,69 @@ if(navClose){
         navMenu.classList.remove('show-menu');
     })
 }
+
+// CHANGE HOME PAGE IMAGE 
+let quant = document.getElementsByClassName('home__image');
+let atual = 0;
+let imagem = document.getElementById('default-img');
+let imagePosition = document.getElementById('img__display')
+let next = document.getElementById('img__change-right');
+let previous = document.getElementById('img__change-left');
+let changing = true
+
+let homeTitle = document.getElementById('home-page-title');
+
+
+previous.addEventListener('click', () => {
+    atual--
+    changeImage()
+    changing = false
+})
+
+next.addEventListener('click', () =>{
+    atual++
+    changeImage()
+    changing = false
+})
+
+function changeImage(){
+    if(atual >= quant.length){
+        atual = 0;
+        
+    }
+    else if(atual < 0){
+        atual = quant.length-1
+    }
+    imagem.style.marginLeft = -100*atual+'%' 
+    console.log(atual)
+}
+
+function changeText(){
+    switch(atual){
+
+        case 0 :
+            homeTitle.innerHTML="<b>Discover</b> your adventure"
+            break;
+        case 1 :
+            homeTitle.innerHTML="<b>Surf</b> your best wave"
+            imagePosition.innerHTML="02"
+            break;
+        
+        case 2 :
+            homeTitle.innerHTML="<b>The</b> Skydiving´s rush"
+            imagePosition.innerHTML="03"
+            break;
+    }
+}
+
+setInterval(() =>{
+    if(changing){
+    atual++ 
+    changeImage()
+    changeText()
+    }
+    else{
+        changing = true
+    }
+}, 4000)
+
